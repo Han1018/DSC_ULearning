27e2aa468cd6e3fd934c63467bd9fc4daafc17f8
han1018 <paul52841018@hotmail.com>
2021-05-15 20:43:27 +0800

    test for sample api  GET:/api/v1/courses

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,8 +2,26 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+// 建立course tree api
+router.get("/api/v1/courses", (req, res, next) => {
+
+  res.json(
+    [
+      {
+        id: 4,
+        name: "資料庫 ",
+        descption: "wiki",
+      },
+      {
+        id: 5,
+        name: "人工智慧 :",
+        descption: "wiki",
+      },
+    ]
+  );
+});
 module.exports = router;
