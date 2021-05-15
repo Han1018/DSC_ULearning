498310161cbe2bfb0f1a3ec574e2cf4b1e19c426
Yuen <emma159752@gmail.com>
2021-05-15 19:54:40 +0800

    hi

---
+++ src/components/displayMajors.vue
@@ -0,0 +1,145 @@
+<template>
+  <v-card>
+    <div
+      align="center"
+      justify="center"
+      >
+      <v-btn class="mx-5 black--text" width="100px" color="white">
+        人工智慧
+      </v-btn>
+      <v-btn class="mx-5 black--text" width="100px" color="white">
+        資工
+      </v-btn>
+      <v-btn class="mx-5 black--text" width="100px" color="white">
+        經濟
+      </v-btn>
+      <v-btn class="mx-5 black--text" width="100px" color="white">
+        機械
+      </v-btn>
+      <v-btn class="mx-5 black--text" width="100px" color="white">
+        日文
+      </v-btn>
+    </div>
+    <v-container>
+      <v-row>
+        <v-col cols="12">
+          <v-card
+            color="#385F73"
+            dark
+            class="mx-auto my-5"
+            >
+            <v-card-title class="text-h5">
+              人工智慧
+            </v-card-title>
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-actions>
+              <v-icon dark>
+                mdi-heart
+              </v-icon>
+              <v-btn text>
+                Learn more
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card
+            color="#1F7087"
+            dark
+            class="mx-auto my-5"
+          >
+            <v-card-title class="text-h5">
+              資工
+            </v-card-title>
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-actions>
+              <v-icon dark>
+                mdi-heart
+              </v-icon>
+              <v-btn text>
+                Learn more
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card
+            color="#952175"
+            dark
+            class="mx-auto my-5"
+            >
+            <v-card-title class="text-h5">
+              經濟
+            </v-card-title>
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-actions>
+              <v-icon dark>
+                mdi-heart
+              </v-icon>
+              <v-btn text>
+                Learn more
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card
+            color="#952175"
+            dark
+            class="mx-auto my-5"
+            >
+            <v-card-title class="text-h5">
+              經濟
+            </v-card-title>
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-actions>
+              <v-icon dark>
+                mdi-heart
+              </v-icon>
+              <v-btn text>
+                Learn more
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+          <v-card
+            color="#952175"
+            dark
+            class="mx-auto my-5"
+            >
+            <v-card-title class="text-h5">
+              經濟
+            </v-card-title>
+            <v-card-subtitle>Listen to your favorite artists and albums whenever and wherever, online and offline.</v-card-subtitle>
+            <v-card-actions>
+              <v-icon dark>
+                mdi-heart
+              </v-icon>
+              <v-btn text>
+                Learn more
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-col>
+      </v-row>
+    </v-container>
+    <v-footer>
+      <v-card
+        flat
+        tile
+        width="100%"
+        height="100%"
+        class="red lighten-1 text-center"
+        >
+        <v-card-text class="white--text">
+            <strong>DSC</strong>
+        </v-card-text>
+        <v-divider></v-divider>
+        <v-card-text class="white--text">
+          <strong>{{ new Date().getFullYear() }}</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Home",
+  components: {
+  },
+};
+</script>
---
+++ src/components/login_first.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-card class="elevation-8 pt-12 pb-12 pl-6 pr-6" height="100%">
+    <v-row align="center" justify="center text-center">
+      <v-col cols="12" sm="8" md="4">
+      <v-img height="150" contain src="https://pbs.twimg.com/profile_images/1182361454072209408/VEaUkAkk_400x400.png"></v-img>
+        <h3><strong>LOGIN</strong></h3>
+        <div>
+          <v-text-field
+          label="Account"
+          hide-details="auto"
+          solo
+          class="my-2"
+          clearable
+          ></v-text-field>
+          <v-text-field label="Password" solo clearable class="my-2"></v-text-field>
+        </div>
+      </v-col>
+    </v-row>
+    <div class="text-center">
+      <div class="my-2">
+        <v-btn class="red lighten-1 white--text">
+        Login
+        </v-btn>
+      </div>
+      <div class="my-2">
+        <v-btn class="red lighten-1 white--text">
+        Forget password
+        </v-btn>
+      </div>
+    </div>
+    <v-footer position: fixed>
+      <v-card
+        flat
+        tile
+        width="100%"
+        height="100%"
+        class="red lighten-1 text-center"
+      >
+        <v-card-text class="white--text">
+            <strong>DSC</strong>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-text class="white--text">
+          <strong>{{ new Date().getFullYear() }}</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: "Home",
+
+  components: {},
+};
+</script>
