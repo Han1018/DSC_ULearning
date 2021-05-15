36b12c05cb53f7022130cb3d758a60d7e9fde830
han1018 <paul52841018@hotmail.com>
2021-05-15 22:02:22 +0800

    Temp finish fake(not in db) API : GET=>/api/v1/majors

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -22,4 +22,23 @@ router.get("/api/v1/courses", (req, res, next) => {
   }
   res.json(result);
 });
+
+router.get("/api/v1/majors", (req, res, next) => {
+  var majorList = ["資訊工程", "國際貿易", "經濟", "機械工程", "日語文學", "英語文學", "設計", "心理學"]
+  var result = []
+
+  for (let i = 1; i < majorList.length + 1; i++) {
+    result.push(
+      {
+        id: i,
+        major: majorList[i]
+      }
+    );
+  }
+  res.json(result);
+});
+
+
+
+
 module.exports = router;
