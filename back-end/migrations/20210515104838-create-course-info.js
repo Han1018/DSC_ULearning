c3c197e545da5d7cfccc1519a20d6fd2f1195376
han1018 <paul52841018@hotmail.com>
2021-05-15 19:19:49 +0800

    mirgration init

---
+++ back-end/migrations/20210515104838-create-course-info.js
@@ -0,0 +1,50 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('test_course_infos', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+        unique: true,
+      },
+      image: {
+        type: Sequelize.STRING(200),
+        defaultValue: null,
+        allowNull: true,
+      },
+      title: {
+        type: Sequelize.STRING(50),
+        defaultValue: "",
+        allowNull: false,
+      },
+      sourceName: {
+        type: Sequelize.STRING(50),
+        defaultValue: "",
+        allowNull: true,
+      },
+      link: {
+        type: Sequelize.STRING(200),
+        defaultValue: "",
+        allowNull: false,
+      },
+      description: {
+        type: Sequelize.TEXT,
+        defaultValue: "",
+        allowNull: true,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('test_course_infos');
+  }
+};
