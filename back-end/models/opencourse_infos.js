74e99d94d956fe569ad0b9e5dba14d20e4e4a042
han1018 <paul52841018@hotmail.com>
2021-05-15 19:28:18 +0800

    new a ORM Model - opencourse_info

---
+++ back-end/models/opencourse_infos.js
@@ -0,0 +1,46 @@
+'use strict';
+const { Model } = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  return sequelize.define('opencourse_infos', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER,
+      unique: true,
+    },
+    image: {
+      type: Sequelize.STRING(200),
+      defaultValue: null,
+      allowNull: true,
+    },
+    title: {
+      type: Sequelize.STRING(50),
+      defaultValue: "",
+      allowNull: false,
+    },
+    sourceName: {
+      type: Sequelize.STRING(50),
+      defaultValue: "",
+      allowNull: true,
+    },
+    link: {
+      type: Sequelize.STRING(200),
+      defaultValue: "",
+      allowNull: false,
+    },
+    description: {
+      type: Sequelize.TEXT,
+      defaultValue: "",
+      allowNull: true,
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  })
+};
