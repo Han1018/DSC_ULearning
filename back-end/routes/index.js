1948347dd6441325b0cdfeaef62d0572c183ea99
han1018 <paul52841018@hotmail.com>
2021-05-16 11:54:47 +0800

    cors pb

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -6,7 +6,8 @@ var app = express();
 //设置跨域访问
 app.all('*', function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Headers", "X-Content-Type, Accept, Authorization, X-Requested-With, Origin, Accept-With");
+  res.header('Access-Control-Allow-Credentials', 'false')
   res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
   res.header("X-Powered-By", ' 3.2.1')
   res.header("Content-Type", "application/json;charset=utf-8");
