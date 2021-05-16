384f284da6d4f21518fa2bf73d2a6f8e45eee759
han1018 <paul52841018@hotmail.com>
2021-05-16 11:46:50 +0800

    cors pb

--- back-end/routes/index.js
+++ back-end/routes/index.js
@@ -2,6 +2,18 @@ var express = require('express');
 var router = express.Router();
 const models = require('../models');
 
+var app = express();
+//设置跨域访问
+app.all('*', function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+  res.header("X-Powered-By", ' 3.2.1')
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
+
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
@@ -47,6 +59,7 @@ router.get("/api/v1/majors/:id", async (req, res, next) => {
         }
       );
     }
+    res.status(200);
     res.json(result);
 
   } catch (error) {
